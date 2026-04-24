16c82c528e597c5b728d69d835c4960d41a928da
jan-baranski <46931373+jan-baranski@users.noreply.github.com>
2026-04-24 19:32:23 +0200

    update demo data

--- src/domain/constants.ts
+++ src/domain/constants.ts
@@ -3,4 +3,4 @@ export const MODULE_HEIGHT_PX = 120;
 export const ROW_GAP = 48;
 export const TERMINAL_HIT_SIZE = 11;
 export const EXTERNAL_ZONE_WIDTH_PX = 260;
-export const APP_VERSION = "1.0.4";
+export const APP_VERSION = "1.0.5";
